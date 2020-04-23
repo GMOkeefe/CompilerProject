3cce71f970e95e953eb6c22b2dd8b8377e3dddbc
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-04-22 23:54:27 -0700

    added file reading and input cleaning

--- src/constant.rkt
+++ src/constant.rkt
@@ -2,3 +2,17 @@
 
 (define source (open-input-file "../example/constant.mza"))
 
+(define (while/read [prev : (U EOF String)]) : String
+  (cond
+    [(eof-object? prev) ""]
+    [else (string-append prev (while/read (read-line source 'return-linefeed)))]))
+
+(define (clean [text : (Listof String)]) : (Listof String)
+  (filter
+   (λ ([elem : String]) : Boolean
+     (not (equal? elem "")))
+   text))
+
+(define text (while/read ""))
+(string->symbol text)
+(clean (string-split text " "))
