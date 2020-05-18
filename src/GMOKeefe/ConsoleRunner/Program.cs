a368810daa4762dce2515eb149f6e430df6590de
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-05-18 16:44:33 -0700

    updating c# project to have better file structure and xunit

---
+++ src/GMOKeefe/ConsoleRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GMOKeefe.Compiler.Lexer;
+
+namespace GMOKeefe.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullReader fr = new FullReader("./example/constant.mza");
+            Console.WriteLine(fr.Read());
+        }
+    }
+}
